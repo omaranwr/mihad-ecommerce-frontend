ce75501e677ba2206bcd8b1ed46cbe4580f5976c
Omar Anwar <anwrmr649@gmail.com>
2026-04-01 20:26:03 +0200

    setup custom fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Playfair Display",
+      cssVariable: "--font-serif",
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Outfit",
+      cssVariable: "--font-sans",
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Roboto Mono",
+      cssVariable: "--font-mono",
+    },
+  ],
 });
