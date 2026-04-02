5e3aac8f38775765907a1dd5bb1f9dc7c5522b6d
Omar Anwar <anwrmr649@gmail.com>
2026-04-02 16:20:11 +0200

    add sidebar

---
+++ src/components/MenuButton.tsx
@@ -0,0 +1,34 @@
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet";
+import { links } from "@/lib/constants";
+
+function MenuButton() {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu />
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="text-primary-foreground bg-black/50 backdrop-blur-xl"
+      >
+        <SheetHeader>
+          <div className="px-3 pt-6">
+            <a href="/" className="font-heading text-xl">
+              Logo
+            </a>
+          </div>
+        </SheetHeader>
+        <ul className="flex flex-col gap-3 px-10 py-8 text-lg">
+          {links.map((link, index) => (
+            <li key={index}>
+              <a href={link.href}>{link.name}</a>
+            </li>
+          ))}
+        </ul>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default MenuButton;
---
+++ src/lib/constants.ts
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    name: "About us",
+    href: "/about",
+  },
+  {
+    name: "FAQs",
+    href: "/faq",
+  },
+  {
+    name: "Products",
+    href: "/products",
+  },
+  {
+    name: "Collections",
+    href: "/collections",
+  },
+  {
+    name: "Contacts",
+    href: "/contacts",
+  },
+] as const;
