3cfaae9b935d38c1ce9bcc56e5204746c94df779
Omar Anwar <anwrmr649@gmail.com>
2026-04-13 12:05:09 +0200

    remove console log

--- src/components/Navbar/NavbarWrapper.tsx
+++ src/components/Navbar/NavbarWrapper.tsx
@@ -7,7 +7,6 @@ function NavbarWrapper({ children }: { children: ReactNode }) {
       setIsOnTop(window.scrollY <= 30);
     });
   }, []);
-  console.log(isOnTop);
   return (
     <div
       className="data-[top=true]:bg-primary/67 text-primary-foreground group bg-primary/20 shadow-primary/20 fixed inset-x-0 z-1 shadow backdrop-blur-md transition-colors"
