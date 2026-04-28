fc8f02f57b395f473ee9c19fe81d17c1b36b5723
Omar Anwar <anwrmr649@gmail.com>
2026-04-28 20:55:34 +0300

    add products and make product take image

--- src/components/ProductsSection/Product.tsx
+++ src/components/ProductsSection/Product.tsx
@@ -4,15 +4,22 @@ interface Props {
   price: number;
   originalPrice?: number;
   name: string;
+  image: string | null;
 }
 
-const Product = ({ price, originalPrice, name }: Props) => {
+const Product = ({ price, originalPrice, name, image }: Props) => {
   return (
     <Card className="group/product">
       <CardContent>
         <div className="relative aspect-3/4 w-full overflow-hidden rounded">
           <div className="absolute inset-0 z-1 bg-red-600 transition-opacity duration-250 group-hover/product:opacity-0"></div>
-          <div className="absolute inset-0 bg-gray-600"></div>
+          {image && (
+            <img
+              className="absolute inset-0 h-full w-full bg-gray-600 object-cover"
+              src={image}
+              alt={name}
+            />
+          )}
         </div>
         <h2 className="pt-3 pb-2 text-2xl">{name}</h2>
         <h3 className="text-xl">
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,7 @@
-import { category } from "@/db/schema";
+import { category, product, productImages, productVariant } from "@/db/schema";
 import { TURSO_AUTH_TOKEN, TURSO_DATABASE_URL } from "astro:env/server";
 import { drizzle } from "drizzle-orm/libsql";
+import { eq } from "drizzle-orm";
 
 export const db = drizzle({
   connection: {
@@ -10,8 +11,23 @@ export const db = drizzle({
 });
 
 export async function getCategories() {
-  const categories = await db
-    .select({ name: category.categoryName })
-    .from(category);
-  return categories.map((c) => c.name);
+  const categories = await db.select().from(category);
+  return categories;
 }
+
+export async function getCategoryById(id: number) {
+  const result = await db.select().from(category).where(eq(category.id, id));
+  if (result.length === 0) return null;
+  return result[0];
+}
+
+export async function getProducts() {
+  const products = await db
+    .select()
+    .from(product)
+    .leftJoin(productVariant, eq(productVariant.productId, product.id))
+    .leftJoin(productImages, eq(productImages.productId, product.id));
+  return products;
+}
+
+export async function getProductsByCategoryId(categoryId: string) {}
