edb904a16f2194d3bb11675286897bdc380ec586
Omar Anwar <anwrmr649@gmail.com>
2026-05-24 14:59:12 +0300

    Revert "setup drizzle orm and make schema"
    This reverts commit fa9df18efd8f8050fe7e8ba6370927785e52074b.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,98 +1,34 @@
-import {
-  category,
-  product,
-  productBaseImage,
-  productFrontImage,
-  productImages,
-  productVariant,
-} from "@/db/schema";
-import { TURSO_AUTH_TOKEN, TURSO_DATABASE_URL } from "astro:env/server";
-import { drizzle } from "drizzle-orm/libsql";
-import { and, eq, sql } from "drizzle-orm";
 import type { ProductCard } from "./types";
 
-export const db = drizzle({
-  connection: {
-    url: TURSO_DATABASE_URL,
-    authToken: TURSO_AUTH_TOKEN,
-  },
-});
-
+// TODO
 export async function getCategories() {
-  const categories = await db.select().from(category);
-  return categories;
+  return [
+    { name: "Suits", id: 1 },
+    { name: "Blouses", id: 2 },
+    { name: "Shirts", id: 3 },
+    { name: "Dresses", id: 4 },
+    { name: "Trousers", id: 5 },
+    { name: "Summer collection", id: 6 },
+    { name: "Winter collection", id: 7 },
+  ];
 }
 
+// TODO
 export async function getCategoryById(id: number) {
-  const result = await db.select().from(category).where(eq(category.id, id));
-  if (result.length === 0) return null;
-  return result[0];
+  const categories = (await getCategories()).filter(
+    (category) => category.id === id,
+  );
+  return categories.length !== 0 ? categories[0].name : null;
 }
 
+// TODO
+export async function getProductCardsByCategoryId(
+  id: number,
+): Promise<ProductCard[]> {
+  return [];
+}
+
+// TODO
 export async function getProductCards(): Promise<ProductCard[]> {
-  const variants = db
-    .select({
-      id: productVariant.id,
-      productId: productVariant.productId,
-      price: productVariant.price,
-      rowNumber: sql`row_number() OVER (
-        PARTITION BY ${productVariant.productId} 
-        ORDER BY ${productVariant.price} ASC
-      )`.as("row_number"),
-    })
-    .from(productVariant)
-    .as("variants");
-  const products = await db
-    .select({
-      name: product.productName,
-      price: variants.price,
-      image: productBaseImage.image,
-      frontImage: productFrontImage.image,
-    })
-    .from(product)
-    .leftJoin(
-      productBaseImage,
-      eq(product.productBaseImageId, productBaseImage.id),
-    )
-    .leftJoin(
-      productFrontImage,
-      eq(product.productFrontImageId, productFrontImage.id),
-    )
-    .innerJoin(variants, eq(product.id, variants.productId))
-    .where(eq(variants.rowNumber, 1));
-  return products;
-}
-
-export async function getProductCardsByCategoryId(categoryId: number) {
-  const variants = db
-    .select({
-      id: productVariant.id,
-      productId: productVariant.productId,
-      price: productVariant.price,
-      rowNumber: sql`row_number() OVER (
-        PARTITION BY ${productVariant.productId} 
-        ORDER BY ${productVariant.price} ASC
-      )`.as("row_number"),
-    })
-    .from(productVariant)
-    .as("variants");
-  const products = await db
-    .select({
-      name: product.productName,
-      price: variants.price,
-      image: productBaseImage.image,
-      frontImage: productFrontImage.image,
-    })
-    .from(product)
-    .leftJoin(
-      productBaseImage,
-      eq(product.productBaseImageId, productBaseImage.id),
-    )
-    .leftJoin(
-      productFrontImage,
-      eq(product.productFrontImageId, productFrontImage.id),
-    )
-    .innerJoin(variants, eq(product.id, variants.productId))
-    .where(and(eq(variants.rowNumber, 1), eq(product.categoryId, categoryId)));
-  return products;
+  return [];
 }
