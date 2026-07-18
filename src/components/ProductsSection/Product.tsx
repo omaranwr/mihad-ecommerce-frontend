1987916acba8262ad37a5d269d8ca4dfec312c96
Omar Anwar <anwrmr649@gmail.com>
2026-07-18 18:42:40 +0300

    add routing to singular product pages

--- src/components/ProductsSection/Product.tsx
+++ src/components/ProductsSection/Product.tsx
@@ -14,35 +14,46 @@ const Product = ({
   name,
   backImage,
   frontImage,
-}: ProductCard) => {
+  categorySlug,
+  subcategorySlug,
+  productSlug,
+}: ProductCard & {
+  categorySlug: string;
+  subcategorySlug: string;
+  productSlug: string;
+}) => {
   return (
-    <Card className="group/product">
-      <CardContent>
-        <div className="relative aspect-3/4 w-full overflow-hidden rounded">
-          {frontImage && (
-            <img
-              className="absolute inset-0 z-1 h-full w-full bg-red-600 object-cover transition-opacity duration-250 group-hover/product:opacity-0"
-              src={frontImage}
-              aria-hidden
-            />
-          )}
-          {(backImage || frontImage) && (
-            <img
-              className="absolute inset-0 h-full w-full bg-gray-600 object-cover"
-              src={backImage ? backImage : frontImage!}
-              alt={name}
-            />
-          )}
-        </div>
-        <h2 className="pt-3 pb-2 text-2xl">{name}</h2>
-        <h3 className="text-xl">
-          {originalPrice && (
-            <span className="pe-2 text-sm line-through">£{originalPrice}</span>
-          )}
-          £{price}
-        </h3>
-      </CardContent>
-    </Card>
+    <a href={`/products/${categorySlug}/${subcategorySlug}/${productSlug}`}>
+      <Card className="group/product">
+        <CardContent>
+          <div className="relative aspect-3/4 w-full overflow-hidden rounded">
+            {frontImage && (
+              <img
+                className="absolute inset-0 z-1 h-full w-full bg-red-600 object-cover transition-opacity duration-250 group-hover/product:opacity-0"
+                src={frontImage}
+                aria-hidden
+              />
+            )}
+            {(backImage || frontImage) && (
+              <img
+                className="absolute inset-0 h-full w-full bg-gray-600 object-cover"
+                src={backImage ? backImage : frontImage!}
+                alt={name}
+              />
+            )}
+          </div>
+          <h2 className="pt-3 pb-2 text-2xl">{name}</h2>
+          <h3 className="text-xl">
+            {originalPrice && (
+              <span className="pe-2 text-sm line-through">
+                £{originalPrice}
+              </span>
+            )}
+            £{price}
+          </h3>
+        </CardContent>
+      </Card>
+    </a>
   );
 };
 
